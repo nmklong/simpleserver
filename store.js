6505928079b77b40611e8409ceeddc735abf5fab
Long Nguyen <lnguyen@chefworks.com>
2021-08-21 17:01:26 -0700

    use knex to insert

--- store.js
+++ store.js
@@ -1,13 +1,18 @@
 const knex = require('knex')(require('./knexfile'))
 
 const createProduct = ({name, description, price}) => {
-    console.log(`Adding product: 
-        Name: ${name} 
-        Description: ${description}
-        Price: ${price}
+    console.log(`
+    Adding product: 
+        - Name: ${name} 
+        - Description: ${description}
+        - Price: ${price}
     `);
 
-    return Promise.resolve();
+    return knex('product').insert({
+        name,
+        description,
+        price
+    });
 };
 
 module.exports = {
